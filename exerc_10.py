70f9cb6f94e5be424c519e4baa6a7ab8894ce94a
Fabiomarujo <fabioandersonmarujo@gmail.com>
2025-05-12 21:27:49 -0300

    Exercicio UC3

--- exerc_10.py
+++ exerc_10.py
@@ -9,7 +9,7 @@
 import os
 from pyfiglet import Figlet
 from colorama import Fore,Back,Style
-from exercicio_10 import adicionar_itens,RelatorioPraia,Voltar_Sair
+from exercicio_10 import adicionar_itens,RelatorioPraia,Dist_Praia,Voltar_Sair
 os.system("cls")
 f = Figlet()
 print(f.renderText('  SIS PRAIAS'))
@@ -23,7 +23,7 @@ print(titulo)
 nome_usuario = input("\nDigite o nome de usuário: ")
 os.system("cls")
 print(f"Seja bem vindo(a) \033[0;30;36m{nome_usuario}\033[m\n")
-ListaOpcoes = {1:"[1] - Cadastrar Praias ao Dicionário",2:"[2] - Excluir Praia",3:"[3] - Sair"}
+ListaOpcoes = {1:"[1] - Cadastrar Praias ao Dicionário",2:"[2] - Relatório das Praias Cadastradas",3:"[3] - O número de praias que distam mais de 15 km do centro?",4:"[4] - A quantidade média de veranistas, na ultima temporada, nas praias com acesso não asfaltado?",5:"[5] - O nome e a distância do centro, em km, de todas as praias de acesso asfaltado que tiveram menos de 1000 veranistas?",6:"[6] - Sair"}
 Itens = {}
 
 while(True):
@@ -33,13 +33,20 @@ while(True):
     comando = int(input("\nDigite a opção > "))
     if comando == 1:
         adicionar_itens(Itens)
-        print(Itens)
-        
+        print(Itens)        
     elif comando == 2:
         RelatorioPraia(Itens)
         print(Itens)
-        
     elif comando == 3:
+        Dist_Praia(Itens)
+        print(Itens)
+    elif comando == 4:
+        M_Veran_1_Praia(Itens)
+        print(Itens)
+    elif comando == 5:
+        N_Praia(Itens)
+        print(Itens)
+    elif comando == 6:
         print(Fore.YELLOW + "\nAguarde! Deslogando...\n" + Fore.WHITE)
         os.abort()  
 
--- exercicio_10.py
+++ exercicio_10.py
@@ -1,26 +1,44 @@
 import os
 from pyfiglet import Figlet
 from colorama import Fore,Back,Style
-
+Itens_Carrinho = {}
 def adicionar_itens(dicionario_itens):
     os.system("cls")
     print(Fore.GREEN + "*** CADASTRAR - PRAIAS ***\n" + Fore.WHITE)
-    print("Limite de cadastro de 4 Praias por evento.\n")
-    Itens_Carrinho = {}
-    Template_dados = {"dist_centro":None,"n_veranistas":None,"t_acesso":None}
-    cont = 0
-    for i in range(0,4):
-        cont = cont+1
-        Nome_Produto = input("\nQual o nome da PRAIA para cadastrar? ")
-        Itens_Carrinho[Nome_Produto] = Template_dados.copy()
-        Template_dados["dist_centro"] = input("Qual a distância do Centro? ")
-        Template_dados["n-veranistas"] = input("Qual a média de veranistas desta Praia? ")
-        Template_dados["t_acesso"] = input("Entre com 0 - Acesso não asfaltado ou 1 - Acesso asfaltado: ")
+    print("Limite de cadastro de 4 Praias por evento.\n")    
+    id = 0
+    for i in range(0,3):
+        id = id+1              
+        Template_dados = {"Nome_Produto":None,"dist_centro":None,"n_veranistas":None,"t_acesso":None}
+        Template_dados["Nome_Produto"] = input("\nQual o nome da PRAIA para cadastrar? ")        
+        Template_dados["dist_centro"] = input("Qual a distância em Km para o Centro? ")
+        Template_dados["n_veranistas"] = input("Qual a média de veranistas desta Praia? ")
+        Template_dados["t_acesso"] = input("Entre com 0 - Acesso não asfaltado ou 1 - Acesso asfaltado: ")          
+        Itens_Carrinho[id] = Template_dados.copy()
+          
 
 def RelatorioPraia(DicionarioProduto):
     os.system("cls")
     print("*** SISTEMA - RELATÓRIO PRAIAS CADASTRADAS ***\n")
-    
+    DicionarioProduto = Itens_Carrinho
+    print(DicionarioProduto)
+    return DicionarioProduto
+ 
+def Dist_Praia(DistanciaPraia):
+    os.system("cls")
+    print("*** SISTEMA PRAIAS - NÚMERO DE PRAIAS COM MAIS DE 15 KM ***\n")
+    DistanciaPraia = Itens_Carrinho
+   
+    # print(DistanciaPraia[id]["dist_centro"])
+    while(True):         
+        
+        for b in DistanciaPraia:
+            total = sum(DistanciaPraia[slice]["dist_centro"].values())
+            print(total[b])
+        else:
+            print("Erro!")
+        
+
 def Voltar_Sair(L_Opcoes):
     L_Opcoes = {1:"[1] - Voltar ao Menu Inicial",2:"[2] - Sair"}
     while(True):
